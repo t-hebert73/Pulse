fd4ef1abacf938e955b57c0de2db17f65f42809d
Trevor <t_hebert42@hotmail.com>
2014-07-11 00:01:11 -0400

    added sirduke to media page

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -44,13 +44,12 @@
             $("#jquery_jplayer_1").jPlayer({
                 ready: function () {
                     $(this).jPlayer("setMedia", {
-                        title: "Bubble",
-                        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-                        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+                        title: "Sir Duke (Cover) - Pulse",
+                        mp3: "/audio/SirDuke.mp3"
                     });
                 },
                 swfPath: "/js/jplayer",
-                supplied: "m4a, oga"
+                supplied: "mp3"
             });
         });
     </script>
