0bc001386f0622e95b881f3ba0a0b50e14f4cbe3
Max <lazy_boyblue@hotmail.com>
2014-07-24 15:10:59 -0400

    Fixed up some css on the The Band page

--- app/views/theband.blade.php
+++ app/views/theband.blade.php
@@ -1,7 +1,7 @@
 @extends ('layout')
 @section('content')
 
-<h1 class="page-title">About the band</h1>
+<h1 class="page-title">The Band</h1>
 
 <div class="content">
     <div class="container">
@@ -11,26 +11,49 @@
                 We are the leading Motown/Funk band of Niagara. Here to kick ass and chew bubblegum. Kappa
             </p>
 
-            <div class="row" style="text-align: center">
-                <div class="col-md-3 col-sm-3 band-border" style="border-left: 2px solid black; background-color: #CCE2FF">
+            <div class="row  band-border" style="text-align: center">
+                <div class="col-md-3 odd" style="background-color: #CCE2FF">
 
                     <h3>Patrick Shea</h3>
                     <p>Lead Vocals/Keyboard</p>
+                    <p>Pat started playing on a beat-up old classical guitar with steel strings just before
+                        he started high school and it wasn’t long before he discovered he had an inherent love for
+                        playing instruments.  He has played guitar and bass in both cover bands and originals but
+                        he always found himself singing for the projects he was in.
+
+                        With the birth of Pulse, it was decided that the only way to make the music truly
+                        sound right was to integrate a keyboard and Pat is always up for learning a new instrument.
+                        Having never learned to play, he started with the songs on Pulse’s set and took to it right away.
+                        He may be new to the keyboard but his real contribution is in the way of vocals that can cut through
+                        with piercing high notes or slide along a low, slow groove.  Pat is one part of four that make Pulse
+                        such a unique and power sound.
+                    </p>
 
                 </div>
-                <div class="col-md-3 col-sm-3 band-border" style="background-color: #566780">
+                <div class="col-md-3 even" style="background-color: #566780">
 
                     <h3>Marcus Gazzellone</h3>
                     <p>Drums</p>
+                    <p>
+                        Marcus started playing the drums at a young age but it really began during high school that
+                        he was very serious about music and drums.He also has a passion with recording and mixing songs
+                        in his own studio. He was always practising after school and than realize he should join bands.He
+                        and max played in a couple of bands and realzing they had a good chemsitry together. Marcus explores
+                        genres such as to Funk, fusion, motown to progressive rock, reggae and experimental rock. Having played
+                        for quite sometime, he has develop a ear how to approch music in certain ways. Either its his musical drum
+                        parts or approvising the whole time while playing or practising. For Pulse marcus is responsible for keeping
+                        the groove and the pocket going in a timly manner. He gives energy, chemestry, and unique personality to the
+                        band and his chops that you would know its marcus gazzellone.
+                    </p>
 
                 </div>
-                <div class="col-md-3 col-sm-3 band-border" style="background-color: #CCE2FF">
+                <div class="col-md-3 odd" style="background-color: #CCE2FF">
 
                     <h3>Max Saglimbeni</h3>
                     <p>Bass Guitar/Backup Vocals</p>
 
                 </div>
-                <div class="col-md-3 col-sm-3 band-border" style="background-color: #566780">
+                <div class="col-md-3 even" style="background-color: #566780">
 
                     <h3>Trevor Hebert</h3>
                     <p>Lead Guitar</p>
