165f88c339de7beaee1b45f250074a1c86d4c0ed
Max <lazy_boyblue@hotmail.com>
2014-07-25 14:28:28 -0400

    Testing modals

--- app/views/theband.blade.php
+++ app/views/theband.blade.php
@@ -7,46 +7,55 @@
     <div class="container">
 
         <div class="mainbar">
-            <p style="text-align: center">
-                We are the leading Motown/Funk band of Niagara. Here to kick ass and chew bubblegum. Kappa
-            </p>
 
-            <div class="row  band-border" style="text-align: center">
-                <div class="col-md-3 odd" style="background-color: #CCE2FF">
+            <div class="row" style="text-align: center">
 
-                    <h3>Patrick Shea</h3>
-                    <p>Lead Vocals/Keyboard</p>
-                    <p>Pat started playing on a beat-up old classical guitar with steel strings just before
-                        he started high school and it wasn’t long before he discovered he had an inherent love for
-                        playing instruments.  He has played guitar and bass in both cover bands and originals but
-                        he always found himself singing for the projects he was in.
+                <div class="col-md-3" style="background-color: #CCE2FF">
+                    <a href="#openModalPat">Patrick Shea</a>
 
-                        With the birth of Pulse, it was decided that the only way to make the music truly
-                        sound right was to integrate a keyboard and Pat is always up for learning a new instrument.
-                        Having never learned to play, he started with the songs on Pulse’s set and took to it right away.
-                        He may be new to the keyboard but his real contribution is in the way of vocals that can cut through
-                        with piercing high notes or slide along a low, slow groove.  Pat is one part of four that make Pulse
-                        such a unique and power sound.
-                    </p>
+                    <div id="openModalPat" class="modalDialog">
+                        <div>
+                            <a href="#close" title="Close" class="close">X</a>
+                            <h3>Patrick Shea</h3>
+                            <p>Lead Vocals/Keyboard</p>
+                            <p>Pat started playing on a beat-up old classical guitar with steel strings just before
+                                he started high school and it wasn’t long before he discovered he had an inherent love for
+                                playing instruments.  He has played guitar and bass in both cover bands and originals but
+                                he always found himself singing for the projects he was in.
 
+                                With the birth of Pulse, it was decided that the only way to make the music truly
+                                sound right was to integrate a keyboard and Pat is always up for learning a new instrument.
+                                Having never learned to play, he started with the songs on Pulse’s set and took to it right away.
+                                He may be new to the keyboard but his real contribution is in the way of vocals that can cut through
+                                with piercing high notes or slide along a low, slow groove.  Pat is one part of four that make Pulse
+                                such a unique and power sound.
+                            </p>
+                        </div>
+                    </div>
                 </div>
+
                 <div class="col-md-3 even" style="background-color: #566780">
-
-                    <h3>Marcus Gazzellone</h3>
-                    <p>Drums</p>
-                    <p>
-                        Marcus started playing the drums at a young age but it really began during high school that
-                        he was very serious about music and drums.He also has a passion with recording and mixing songs
-                        in his own studio. He was always practising after school and than realize he should join bands.He
-                        and max played in a couple of bands and realzing they had a good chemsitry together. Marcus explores
-                        genres such as to Funk, fusion, motown to progressive rock, reggae and experimental rock. Having played
-                        for quite sometime, he has develop a ear how to approch music in certain ways. Either its his musical drum
-                        parts or approvising the whole time while playing or practising. For Pulse marcus is responsible for keeping
-                        the groove and the pocket going in a timly manner. He gives energy, chemestry, and unique personality to the
-                        band and his chops that you would know its marcus gazzellone.
-                    </p>
-
+                    <a href="#openModalMarcus">Marcus Gazzellone</a>
+                    <div id="openModalMarcus" class="modalDialog">
+                        <div>
+                            <a href="#close" title="Close" class="close">X</a>
+                            <h3>Marcus Gazzellone</h3>
+                            <p>Drums</p>
+                            <p>
+                                Marcus started playing the drums at a young age but it really began during high school that
+                                he was very serious about music and drums.He also has a passion with recording and mixing songs
+                                in his own studio. He was always practising after school and than realize he should join bands.He
+                                and max played in a couple of bands and realizing they had a good chemistry together. Marcus explores
+                                genres such as to Funk, fusion, motown to progressive rock, reggae and experimental rock. Having played
+                                for quite sometime, he has develop a ear how to approach music in certain ways. Either its his musical drum
+                                parts or improvising the whole time while playing or practising. For Pulse marcus is responsible for keeping
+                                the groove and the pocket going in a timely manner. He gives energy, chemistry, and unique personality to the
+                                band and his chops that you would know its marcus gazzellone.
+                            </p>
+                        </div>
+                    </div>
                 </div>
+
                 <div class="col-md-3 odd" style="background-color: #CCE2FF">
 
                     <h3>Max Saglimbeni</h3>
@@ -61,7 +70,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 
