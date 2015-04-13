670b980cf3084564a13c956990ddde34eb49d58d
Trevor <t.hebert73@gmail.com>
2015-04-12 21:02:40 -0400

    moved print image function to general functions file

--- app/libraries/general_functions.php
+++ app/libraries/general_functions.php
@@ -13,3 +13,34 @@ function pr($arr) {
     print_r($arr);
     if (!$cli) echo "</pre>";
 }
+
+function PrintImage($target, $srcThumb, $srcFull){
+
+    $image = '<div class="img-box">';
+
+    $image .= '<a href="#" data-toggle="modal" data-target="#'.$target.'">';
+    $image .= '';
+
+    $image .= '<img src="'.$srcThumb.'">';
+    $image .= '</a>';
+
+    $image .= '<div class="modal fade" id="'.$target.'" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">';
+    $image .= '';
+    $image .= '<div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="myModalLabel">Pulse @ Mahtey, January 30/2015</h4>
+                        </div>
+                        <div class="modal-body">
+                            <img src="'.$srcFull.'">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>';
+    $image .= '</div>';
+    $image .= '</div>';
+
+    return $image;
+}
--- app/views/gallery.blade.php
+++ app/views/gallery.blade.php
@@ -1,32 +1,3 @@
-<?php
-
-function PrintImage($target, $srcThumb, $srcFull){
-    echo "<div class='img-box'>";
-
-    echo "<a href='#' data-toggle='modal' data-target='#".$target."'>";
-        echo "<img src='".$srcThumb."'>";
-    echo "</a>";
-
-    echo "<div class='modal fade' id='".$target."' tabindex='-1' role='dialog' aria-labelledby='basicModal' aria-hidden='true'>";
-        echo "<div class='modal-dialog'>
-                    <div class='modal-content'>
-                        <div class='modal-header'>
-                            <h4 class='modal-title' id='myModalLabel'>Pulse @ Mahtey, January 30/2015</h4>
-                        </div>
-                        <div class='modal-body'>
-                            <img src='".$srcFull."'>
-                        </div>
-                        <div class='modal-footer'>
-                            <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-                        </div>
-                    </div>
-                </div>";
-        echo "</div>";
-    echo "</div>";
-}
-
-?>
-
 @extends('layout')
 
 @section('content')
@@ -40,10 +11,10 @@ function PrintImage($target, $srcThumb, $srcFull){
             <div class="gallery-container">
 
                 <?php
-                    PrintImage("img1", "../../images/mahteyjan30/thumbs/img1-thumb.jpg", "../../images/mahteyjan30/img1.jpg");
-                    PrintImage("img2", "../../images/mahteyjan30/thumbs/img2-thumb.jpg", "../../images/mahteyjan30/img2.jpg");
-                    PrintImage("img3", "../../images/mahteyjan30/thumbs/img3-thumb.jpg", "../../images/mahteyjan30/img3.jpg");
-                    PrintImage("img4", "../../images/mahteyjan30/thumbs/img4-thumb.jpg", "../../images/mahteyjan30/img4.jpg");
+                    echo PrintImage("img1", "../../images/mahteyjan30/thumbs/img1-thumb.jpg", "../../images/mahteyjan30/img1.jpg");
+                    echo PrintImage("img2", "../../images/mahteyjan30/thumbs/img2-thumb.jpg", "../../images/mahteyjan30/img2.jpg");
+                    echo PrintImage("img3", "../../images/mahteyjan30/thumbs/img3-thumb.jpg", "../../images/mahteyjan30/img3.jpg");
+                    echo PrintImage("img4", "../../images/mahteyjan30/thumbs/img4-thumb.jpg", "../../images/mahteyjan30/img4.jpg");
                 ?>
 
                 <div class="clear"></div>
