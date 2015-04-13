d74de657a9c747c244929d7c3ce0190703d0fe29
Trevor <t.hebert73@gmail.com>
2015-04-12 21:22:47 -0400

    refactored print image folder

--- app/libraries/general_functions.php
+++ app/libraries/general_functions.php
@@ -14,25 +14,30 @@ function pr($arr) {
     if (!$cli) echo "</pre>";
 }
 
+
+/**
+ * @param $target - html ID target
+ * @param $srcThumb - source from images folder
+ * @param $srcFull - source from images folder
+ * @return string - html modal formatted image
+ */
 function PrintImage($target, $srcThumb, $srcFull){
 
     $image = '<div class="img-box">';
 
     $image .= '<a href="#" data-toggle="modal" data-target="#'.$target.'">';
-    $image .= '';
 
-    $image .= '<img src="'.$srcThumb.'">';
+    $image .= '<img src="../../images/'.$srcThumb.'">';
     $image .= '</a>';
 
     $image .= '<div class="modal fade" id="'.$target.'" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">';
-    $image .= '';
     $image .= '<div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h4 class="modal-title" id="myModalLabel">Pulse @ Mahtey, January 30/2015</h4>
                         </div>
                         <div class="modal-body">
-                            <img src="'.$srcFull.'">
+                            <img src="../../images/'.$srcFull.'">
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
--- app/views/gallery.blade.php
+++ app/views/gallery.blade.php
@@ -11,10 +11,10 @@
             <div class="gallery-container">
 
                 <?php
-                    echo PrintImage("img1", "../../images/mahteyjan30/thumbs/img1-thumb.jpg", "../../images/mahteyjan30/img1.jpg");
-                    echo PrintImage("img2", "../../images/mahteyjan30/thumbs/img2-thumb.jpg", "../../images/mahteyjan30/img2.jpg");
-                    echo PrintImage("img3", "../../images/mahteyjan30/thumbs/img3-thumb.jpg", "../../images/mahteyjan30/img3.jpg");
-                    echo PrintImage("img4", "../../images/mahteyjan30/thumbs/img4-thumb.jpg", "../../images/mahteyjan30/img4.jpg");
+                    echo PrintImage("img1", "mahteyjan30/thumbs/img1-thumb.jpg", "mahteyjan30/img1.jpg");
+                    echo PrintImage("img2", "mahteyjan30/thumbs/img2-thumb.jpg", "mahteyjan30/img2.jpg");
+                    echo PrintImage("img3", "mahteyjan30/thumbs/img3-thumb.jpg", "mahteyjan30/img3.jpg");
+                    echo PrintImage("img4", "mahteyjan30/thumbs/img4-thumb.jpg", "mahteyjan30/img4.jpg");
                 ?>
 
                 <div class="clear"></div>
