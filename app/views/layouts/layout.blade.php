40bd00706c3dddb8f89aafeb37b03bbe7be78a41
Trevor <t.hebert73@gmail.com>
2015-04-18 17:48:48 -0400

    switched to using request_uri over php_self for page classes

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -54,7 +54,7 @@
     <?php
         //Get the current page name
         global $currentPage;
-        $currentPage = basename($_SERVER['PHP_SELF']);
+        $currentPage = basename($_SERVER['REQUEST_URI']);
     ?>
 
 </head>
