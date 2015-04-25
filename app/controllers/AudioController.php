b53d5dfb282752dbe6c6c77bc6e981899afe5b50
Max <lazy_boyblue@hotmail.com>
2015-04-25 11:33:37 -0400

    pulse: Added migration for audio table. Created modal and Controller for audio uploading. Controller still in progress/incomplete.

---
+++ app/controllers/AudioController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 4/25/15
+ * Time: 10:27 AM
+ */
+
+class AudioController extends \BaseController{
+
+    public function index(){
+
+        $audio = Audio::all();
+
+        return View::make('audio.index')->with('audio', $audio);
+    }
+
+    public function create(){
+
+        return View::make('audio.create');
+
+    }
+
+    public function store(){
+
+        $rules = array(
+            'audio' => array('required', 'audio')
+        );
+
+        $input = Input::all();
+
+        $validator = Validator::make($input, $rules);
+
+        if($validator->fails()){
+
+        }else{
+            $audio = new Audio;
+
+            $audio->file_name = $input['audio']->getClientOriginalName();
+
+            $input['audio']->move('public/audio/uploaded', $audio->file_name);
+
+            $audio->save();
+
+            Session::flash('message', 'Audio file successfully uploaded');
+            return Redirect::to('audio.create');
+        }
+    }
+}
---
+++ app/models/Audio.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 4/25/15
+ * Time: 10:28 AM
+ */
+
+class Audio extends Eloquent{
+    protected $fillable = ['file_name'];
+}
